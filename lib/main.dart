74629d9660d0715c76b9369bde26d9dc93a0bb8e
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-08-23 20:36:24 +0300

    add theme to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:gamerentz/theme/theme_constants.dart';
+import 'package:gamerentz/theme/theme_manager.dart';
+import 'package:gamerentz/views/buyers/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+ThemeManager themeManager = ThemeManager();
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+    themeManager.removeListener(themeListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    themeManager.addListener(() {
+      themeListener();
+    });
+    super.initState();
+  }
+
+  themeListener() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(),
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: themeManager.themeMode,
+      home: const MainScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
