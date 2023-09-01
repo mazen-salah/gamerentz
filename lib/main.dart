3a499ae6f0f58b670d6910ce3556c349a67ef8dc
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-01 11:05:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'providers/bottom_bar_provider.dart';
 import 'firebase_options.dart';
 import 'package:gamerentz/theme/theme_constants.dart';
-import 'package:gamerentz/theme/theme_manager.dart';
+import 'package:gamerentz/providers/theme_provider.dart';
 import 'package:gamerentz/views/buyers/main_screen.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeManager()),
+        ChangeNotifierProvider(create: (_) => BottomNavigationBarProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void dispose() {
-    themeManager.removeListener(themeListener);
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    themeManager.addListener(() {
-      themeListener();
-    });
-    super.initState();
-  }
-
-  themeListener() {
-    if (mounted) {
-      setState(() {});
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'GameRentz',
       theme: lightTheme,
       darkTheme: darkTheme,
-      themeMode: themeManager.themeMode,
+      themeMode: Provider.of<ThemeManager>(context).themeMode,
       home: const MainScreen(),
     );
   }
