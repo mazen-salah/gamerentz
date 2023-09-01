94fe9185647327584f7d855193e1f55ed9d1320d
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-01 11:05:16 +0300

    Update main_screen.dart

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:gamerentz/theme/theme_manager.dart';
+import 'package:provider/provider.dart';
+import 'package:gamerentz/views/buyers/settings_screen.dart';
+import '../../providers/bottom_bar_provider.dart';
+import 'bottom_bar.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final tabManager = Provider.of<BottomNavigationBarProvider>(context);
+
+    final screens = [
+      const Center(child: Text('Home')),
+      const Center(child: Text('Search')),
+      const Center(child: Text('Cart')),
+      const Settings(),
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: BottomBar(),
+      ),
+      body: screens[tabManager.currentIndex],
+    );
   }
 }
-
