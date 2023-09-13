2153fa295d31854d623a210b63c4ef7e21a0d30a
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-13 23:24:55 +0300

    Update show_snackbar.dart

--- lib/utils/show_snackbar.dart
+++ lib/utils/show_snackbar.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 showSnack(context, String title) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
+      backgroundColor:
+          title.contains("success") || title.contains("successfully")
+              ? Colors.green
+              : null,
       content: Text(
         title,
         style: const TextStyle(
