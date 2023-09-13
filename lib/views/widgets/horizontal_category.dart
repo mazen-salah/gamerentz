d988ea242f0def1e81033c1ca4363b23188bea3b
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-13 23:24:21 +0300

    Update horizontal_category.dart

--- lib/views/widgets/horizontal_category.dart
+++ lib/views/widgets/horizontal_category.dart
@@ -1,50 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:gamerentz/utils/responsive.dart';
+import 'package:gamerentz/models/game_model.dart';
 import 'package:gamerentz/views/widgets/game_card.dart';
 
-class HorizontalCategory extends StatelessWidget {
+class HorizontalCategory extends StatefulWidget {
   const HorizontalCategory({
     Key? key,
     required this.title,
-    required this.itemCount,
+    required this.collectionName,
+    this.sortBy = '',
   }) : super(key: key);
 
   final String title;
-  final int itemCount;
+  final String collectionName;
+  final String sortBy;
+
+  @override
+  State<HorizontalCategory> createState() => _HorizontalCategoryState();
+}
+
+class _HorizontalCategoryState extends State<HorizontalCategory> {
+  late Future<List<Game>> _categoryData;
+
+  @override
+  void initState() {
+    super.initState();
+    _categoryData = _fetchCategoryData();
+  }
+
+  Future<List<Game>> _fetchCategoryData() async {
+    CollectionReference collection =
+        FirebaseFirestore.instance.collection(widget.collectionName);
+
+    Query query = widget.sortBy.isEmpty
+        ? collection.limit(10)
+        : collection.orderBy(widget.sortBy, descending: true).limit(10);
+
+    final querySnapshot = await query.get();
+    return querySnapshot.docs.map((document) {
+      return Game(
+        id: document.id,
+        title: document['title'] ?? 'No title available',
+        imageUrl: document['image'] ?? '',
+        coverUrl: document['cover'] ?? '',
+        description: document['description'] ?? 'No description available',
+        screenshots: document['screenshots'] ?? [],
+        platform:  setPlatformFromString(document['platform'] ?? 'ps4')
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final width = Responsive.isDesktop(context)
-        ? MediaQuery.of(context).size.width / 10
-        : MediaQuery.of(context).size.width / 4;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(title, style: Theme.of(context).textTheme.titleSmall),
+            Text(widget.title, style: Theme.of(context).textTheme.titleSmall),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                // Handle the "Show all" button click here
+              },
               label: const Icon(Icons.arrow_forward),
               icon: const Text('Show all'),
             ),
           ],
         ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: List.generate(
-              itemCount,
-              (index) {
-                return GameCard(
-                  width: width,
-                  index: index,
-                  isFav: false,
-                );
-              },
-            ),
-          ),
+        FutureBuilder<List<Game>>(
+          future: _categoryData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Text('No data available');
+            }
+
+            final games = snapshot.data!;
+
+            return SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: games
+                    .map(
+                      (game) => GameCard(
+                        game: game, // Pass the Game object
+                      ),
+                    )
+                    .toList(),
+              ),
+            );
+          },
         ),
       ],
     );
