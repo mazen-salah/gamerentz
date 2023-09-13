e6240765eb40f91908435accc8b6d9083c125be6
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-13 23:24:38 +0300

    Update account_widget.dart

--- lib/views/widgets/account_widget.dart
+++ lib/views/widgets/account_widget.dart
@@ -1,8 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:gamerentz/utils/helper_widgets.dart';
 
-class AccountWidget extends StatelessWidget {
-  const AccountWidget({super.key});
+class AccountWidget extends StatefulWidget {
+  const AccountWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AccountWidget> createState() => _AccountWidgetState();
+}
+
+class _AccountWidgetState extends State<AccountWidget> {
+  late User _currentUser; // Firebase User object
+  late String _displayName;
+  late String _userImageURL;
+  bool _isEditing = false;
+  final TextEditingController _displayNameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData(); // Load user data from Firebase
+  }
+
+  Future<void> _loadUserData() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        _currentUser = user;
+        _displayName = user.displayName ?? "User";
+        _userImageURL = user.photoURL ?? "";
+        _displayNameController.text = _displayName;
+      });
+    }
+  }
+
+  Future<void> _updateUserProfile() async {
+    try {
+      await _currentUser.updateDisplayName(_displayName);
+      await _currentUser.reload();
+      final updatedUser = FirebaseAuth.instance.currentUser;
+      if (updatedUser != null) {
+        setState(() {
+          _currentUser = updatedUser;
+          _displayName = updatedUser.displayName ?? "User";
+          _userImageURL = updatedUser.photoURL ?? "";
+          _displayNameController.text = _displayName;
+          _isEditing = false;
+        });
+      }
+    } catch (error) {
+      if (kDebugMode) {
+        print("Failed to update display name: $error");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +64,69 @@ class AccountWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Profile', style: Theme.of(context).textTheme.titleLarge),
-          verticalSpace(10),
+          const SizedBox(height: 10),
           Row(
             children: [
-              const CircleAvatar(
+              CircleAvatar(
                 radius: 30,
                 backgroundColor: Colors.blue,
+                backgroundImage:
+                    _userImageURL.isEmpty ? null : NetworkImage(_userImageURL),
+                child: _userImageURL.isEmpty
+                    ? const Icon(
+                        Icons.person,
+                        size: 60,
+                        color: Colors.white,
+                      )
+                    : null,
               ),
               const SizedBox(width: 20),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "John Doe",
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _isEditing
+                        ? TextFormField(
+                            controller: _displayNameController,
+                            onChanged: (value) {
+                              setState(() {
+                                _displayName = value;
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              labelText: "Full Name",
+                            ),
+                          )
+                        : Text(
+                            _displayName,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                    const SizedBox(height: 10),
+                    _isEditing
+                        ? ElevatedButton(
+                            onPressed: () {
+                              _updateUserProfile();
+                            },
+                            child: const Text("Save"),
+                          )
+                        : TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isEditing = true;
+                              });
+                            },
+                            child: const Text("Edit Profile"),
+                          ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        FirebaseAuth.instance.signOut();
+                      },
+                      child: const Text("Sign Out"),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
