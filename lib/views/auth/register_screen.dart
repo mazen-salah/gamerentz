0994a01c6b320f45d4a003558ba83eaa4b0c107a
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-13 23:25:09 +0300

    Update register_screen.dart

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,13 +1,16 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gamerentz/controllers/auth_controller.dart';
+import 'package:gamerentz/theme/theme_constants.dart';
 import 'package:gamerentz/utils/show_snackBar.dart';
 import 'package:gamerentz/views/auth/login_screen.dart';
+import 'package:gamerentz/views/screens/main_screen.dart';
 import 'package:image_picker/image_picker.dart';
 
 class BuyerRegisterScreen extends StatefulWidget {
-  const BuyerRegisterScreen({super.key});
+  const BuyerRegisterScreen({Key? key}) : super(key: key);
 
   @override
   State<BuyerRegisterScreen> createState() => _BuyerRegisterScreenState();
@@ -15,63 +18,62 @@ class BuyerRegisterScreen extends StatefulWidget {
 
 class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
   final AuthController _authController = AuthController();
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   late String email;
-
   late String fullName;
-
   late String phoneNumber;
-
   late String password;
-
   bool _isLoading = false;
-
   Uint8List? _image;
 
-  _signUpUser() async {
-    setState(() {
-      _isLoading = true;
-    });
+  Future<void> _signUpUser() async {
     if (_formKey.currentState!.validate()) {
-      await _authController
-          .signUpUSers(email, fullName, phoneNumber, password, _image)
-          .whenComplete(() {
-        setState(() {
-          _formKey.currentState!.reset();
-          _isLoading = false;
-        });
+      setState(() {
+        _isLoading = true;
       });
 
-      if (!mounted) return null;
-      return showSnack(
-          context, 'Account Created Successfully');
-    } else {
+      final signUpResult = await _authController.signUpUser(
+        email,
+        fullName,
+        phoneNumber,
+        password,
+        _image,
+      );
+
       setState(() {
         _isLoading = false;
       });
-      return showSnack(context, 'Please Fields must not be empty');
+
+      if (signUpResult == 'success') {
+        if (!mounted) return;
+        showSnack(context, 'Account Created Successfully');
+        _formKey.currentState!.reset();
+        setState(() {
+          _image = null;
+        });
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => const MainScreen(),
+          ),
+        );
+      } else {
+        if (!mounted) return;
+        showSnack(context, signUpResult);
+      }
+    } else {
+      showSnack(context, 'Please fill in all fields');
     }
   }
 
-  selectGalleryImage() async {
-    Uint8List im = await _authController.pickProfileImage(ImageSource.gallery);
-
+  Future<void> selectImage(ImageSource source) async {
+    final image = await _authController.pickProfileImage(source);
     setState(() {
-      _image = im;
+      _image = image;
     });
   }
 
-  selectCameraImage() async {
-    Uint8List im = await _authController.pickProfileImage(ImageSource.camera);
-
-    setState(() {
-      _image = im;
-    });
-  }
-
-  // _signUpUser() async {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,30 +85,42 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
-                  'Create Customer"s Account',
+                  'Create Account',
                   style: TextStyle(fontSize: 20),
                 ),
                 Stack(
                   children: [
-                    _image != null
-                        ? CircleAvatar(
-                            radius: 64,
-                            backgroundImage: MemoryImage(_image!),
-                          )
-                        : const CircleAvatar(
-                            radius: 64,
-                            backgroundImage: NetworkImage(
-                                'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'),
-                          ),
+                    if (_image != null)
+                      CircleAvatar(
+                        radius: 64,
+                        backgroundImage: MemoryImage(_image!),
+                      )
+                    else
+                      const CircleAvatar(
+                        radius: 64,
+                        backgroundColor: primaryColor,
+                        child: Icon(
+                          CupertinoIcons.person,
+                          color: Colors.white,
+                          size: 100,
+                        ),
+                      ),
                     Positioned(
                       right: 0,
-                      top: 5,
-                      child: IconButton(
-                        onPressed: () {
-                          selectGalleryImage();
-                        },
-                        icon: const Icon(
-                          CupertinoIcons.photo,
+                      top: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            selectImage(ImageSource.gallery);
+                          },
+                          icon: const Icon(
+                            CupertinoIcons.photo,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
@@ -117,10 +131,9 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                   child: TextFormField(
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'Please Email must not be empty';
-                      } else {
-                        return null;
+                        return 'Please enter your email';
                       }
+                      return null;
                     },
                     onChanged: (value) {
                       email = value;
@@ -135,10 +148,9 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                   child: TextFormField(
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'Please Full Name must not be empty';
-                      } else {
-                        return null;
+                        return 'Please enter your full name';
                       }
+                      return null;
                     },
                     onChanged: (value) {
                       fullName = value;
@@ -153,10 +165,9 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                   child: TextFormField(
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'Please Phone Number must not be empty';
-                      } else {
-                        return null;
+                        return 'Please enter your phone number';
                       }
+                      return null;
                     },
                     onChanged: (value) {
                       phoneNumber = value;
@@ -172,10 +183,9 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                     obscureText: true,
                     validator: (value) {
                       if (value!.isEmpty) {
-                        return 'Please Password must not be empty';
-                      } else {
-                        return null;
+                        return 'Please enter your password';
                       }
+                      return null;
                     },
                     onChanged: (value) {
                       password = value;
@@ -194,18 +204,21 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                     height: 50,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).primaryColor,
                     ),
                     child: Center(
-                        child: _isLoading
-                            ? const CircularProgressIndicator()
-                            : const Text(
-                                'Register',
-                                style: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  letterSpacing: 4,
-                                ),
-                              )),
+                      child: _isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text(
+                              'Register',
+                              style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 4,
+                                color: Colors.white,
+                              ),
+                            ),
+                    ),
                   ),
                 ),
                 Row(
@@ -214,15 +227,17 @@ class _BuyerRegisterScreenState extends State<BuyerRegisterScreen> {
                     const Text('Already Have An Account?'),
                     TextButton(
                       onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const LoginScreen();
-                        }));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginScreen(),
+                          ),
+                        );
                       },
                       child: const Text('Login'),
                     ),
                   ],
-                )
+                ),
               ],
             ),
           ),
