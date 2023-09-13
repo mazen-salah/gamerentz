fe37497afa5d7e33ac038dcfae9669b44ba57f71
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-09-13 23:26:04 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gamerentz/providers/my_providers.dart';
-import 'package:gamerentz/views/screens/main_screen.dart';
+import 'package:gamerentz/views/auth/login_screen.dart';
 import 'package:gamerentz/theme/theme_constants.dart';
+import 'package:gamerentz/views/screens/main_screen.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -15,9 +17,13 @@ Future<void> main() async {
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => ThemeManager()),
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
+        StreamProvider<User?>.value(
+          initialData: null, 
+          value: FirebaseAuth.instance.authStateChanges(),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -29,12 +35,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final User? user = Provider.of<User?>(context);
+
+    
     return MaterialApp(
       title: 'GameRentz',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: Provider.of<ThemeManager>(context).themeMode,
-      home: const MainScreen(),
+      home: user == null ? const LoginScreen() : const MainScreen(),
     );
   }
 }
